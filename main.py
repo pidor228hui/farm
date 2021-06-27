4ed94fbf152b1c783caf71974efb6dde723332b8
Ximetov <70310062+ximetov@users.noreply.github.com>
2021-06-27 07:52:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,11 @@ def farm():
         for result in tokenakka:
             vk_session = vk_api.VkApi(token=result)
             api = vk_session.get_api()
-            print(result)
+            print('Комментарий написан!')
             pizda = 0
             while pizda == 0:
                 try:
                     api.wall.createComment(owner_id = -174105461, post_id = 6713149, message = 'Ферма') #не трогать
-                    time.sleep(3)
-                    api.messages.send(user_id = Сюда айди осн акк, где бот будет писать что отправил смс, message = 'Готово', random_id = r.randint(1,2048)) #Eсли отправка смс не нужна, можно удалить эту строку с time.sleep
                     pizda = pizda + 1
                 except Exception as er:
                     pizda = pizda + 1
