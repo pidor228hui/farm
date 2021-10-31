93a185e55bb67d2df14b6a9a306c8dd9150e672d
pidor228hui <78651236+pidor228hui@users.noreply.github.com>
2021-10-31 13:25:09 +0300

    Add files via upload

--- farm.py
+++ farm.py
@@ -4,8 +4,6 @@ import random
 
 from threading import Thread
 
-#nb = '☁Infinity☁'
-#   Вводишь нужное название беседы
 
 def main(tokk):
     while True:
@@ -15,31 +13,9 @@ def main(tokk):
         user_name = f"{vk_session.method('account.getProfileInfo')['first_name']} {vk_session.method('account.getProfileInfo')['last_name']}"
         while True:
             try:
-                #sm = vk.messages.searchConversations(q=f'{nb}')['items'][0]['peer']['id']
-                #time.sleep(random.randint(0, 5))
                 cid = vk.wall.createComment(owner_id=-174105461,
                                             post_id=6713149,
                                             message='ферма')['comment_id']
-                time.sleep(2)
-                try:
-                    rt = vk.wall.getComments(owner_id=-174105461,
-                                             post_id=6713149,
-                                             comment_id=cid)['items'][0]['text']
-                    print(f"[{user_name}] Ответ ириса: {rt}")
-                    #if rt[:1] == '✅':
-                        #try:
-                         #   id_to = vk.messages.send(peer_id=sm,
-                          #                   random_id=0,
-                           #                  message=f'бкоин 100')
-                           # time.sleep(6)
-                           # vk.messages.delete(peer_id=sm, delete_for_all=1, message_ids=id_to)
-                           # print(f"[{user_name}] Сообщение в беседу доставлено\nЗасыпаю.")
-                        except Exception as er_mg:
-                            print(f"[{user_name}] Произошла ошибка {er_mg}\nВероятно, данного аккаунта нету в нужной беседе.")
-                    #else:
-                     #   print('НЕЗАЧЁТ!!!')
-                except:
-                    print(f"[{user_name}] Комментарий оставил, но ответ от ириса не получил!")
             except Exception as er:
                 print(er)
             time.sleep(14500)
